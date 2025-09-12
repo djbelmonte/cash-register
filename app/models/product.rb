75b7f4836dd20034520b4397fc39075c8f76ff8d
Donn Jerico Belmonte <belmontedonnjerico@gmail.com>
2025-09-12 23:33:47 +0800

    Refactored PromotionService to be flexible with regards to promotion and products under a promotion

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  PROMOTIONS = {
+    :buy_1_take_1 => ["GR1"],
+    :fixed_discount => ["SR1"],
+    :percent_discount => ["CF1"],
+  }.freeze
 end
--- app/services/promotion_service.rb
+++ app/services/promotion_service.rb
@@ -6,54 +6,68 @@ class PromotionService
   attr_accessor :basket
 
   def get_total_with_promo_applied
-    [buy_1_get_1_promo_price.to_d, fixed_discount_promo_price.to_d, percent_discount_promo_price.to_d].sum
+    Product::PROMOTIONS.map do |promo, product_codes|
+      case promo
+      when :buy_1_take_1
+        buy_1_get_1_promo_price(product_codes)
+      when :fixed_discount
+        fixed_discount_promo_price(product_codes)
+      when :percent_discount
+        percent_discount_promo_price(product_codes)
+      end
+    end.sum
   end
 
   private
 
-  def buy_1_get_1_promo_price
-    items = basket.basket_items.joins(:product).where(products: { product_code: "GR1" })
+  def buy_1_get_1_promo_price(product_codes)
+    items = basket.basket_items.joins(:product)
+                               .where(products: { product_code: product_codes })
+                               .to_a
+                               .group_by { |item| item.product.product_code }.values
     item_count = items.count
     return 0 if item_count == 0
-    product = items.first.product
+    sum = 0.0.to_d
 
-    items.each_with_index do |item, index|
-      if index.odd? # take 1
-        item.update_attribute(:promotional_price, "0.00".to_d)
-      else
-        item.update_attribute(:promotional_price, product.price)
+    items.each do |group|
+      group.each_with_index do |item, index|
+        if index.odd? # take 1
+          item.update_attribute(:promotional_price, 0.00.to_d)
+          sum += 0.00.to_d
+        else
+          item.update_attribute(:promotional_price, item.product.price)
+          sum += item.product.price
+        end
       end
     end
     
-    items.map(&:promotional_price).sum
+    sum
   end
 
-  def fixed_discount_promo_price
-    items = basket.basket_items.joins(:product).where(products: { product_code: "SR1" })
+  def fixed_discount_promo_price(product_codes)
+    items = basket.basket_items.joins(:product).where(products: { product_code: product_codes })
     item_count = items.count
     return 0 if item_count == 0
-    product = items.first.product
 
     if item_count >= 3
       items.update_all(promotional_price: 4.50.to_d)
     else
-      items.update_all(promotional_price: product.price)
+      items.update_all("promotional_price = products.price")
     end
 
     items.map(&:promotional_price).sum
   end
 
-  def percent_discount_promo_price
-    items = basket.basket_items.joins(:product).where(products: { product_code: "CF1" })
+  def percent_discount_promo_price(product_codes)
+    items = basket.basket_items.joins(:product).where(products: { product_code: product_codes })
     item_count = items.count
     return 0 if item_count == 0
     product = items.first.product
 
     if item_count >= 3
-      promotional_price = (product.price * (2.to_d / 3.to_d)).round(4)
-      items.update_all(promotional_price: promotional_price)
+      items.update_all("promotional_price = ROUND(products.price * (2/3), 4)")
     else
-      items.update_all(promotional_price: product.price)
+      items.update_all("promotional_price = products.price")
     end
 
     items.map(&:promotional_price).sum.round(2)
